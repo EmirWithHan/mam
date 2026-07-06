0146d4e7d16c836a72f004e1b9175261b9f93967
Emirhan <emirwithhan@gmail.com>
2026-07-06 05:21:36 +0300

    Prepare Android release build version 7

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -18,6 +18,40 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
 }
+subprojects {
+    val configureAndroid = {
+        val android = extensions.findByName("android")
+        if (android != null) {
+            try {
+                val compileOptions = android.javaClass.getMethod("getCompileOptions").invoke(android)
+                compileOptions.javaClass.getMethod("setSourceCompatibility", JavaVersion::class.java).invoke(compileOptions, JavaVersion.VERSION_17)
+                compileOptions.javaClass.getMethod("setTargetCompatibility", JavaVersion::class.java).invoke(compileOptions, JavaVersion.VERSION_17)
+            } catch (e: Exception) {
+                // Ignore if methods don't exist
+            }
+        }
+        tasks.withType<JavaCompile> {
+            sourceCompatibility = "17"
+            targetCompatibility = "17"
+        }
+        try {
+            tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+                compilerOptions {
+                    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+                }
+            }
+        } catch (e: Throwable) {
+            // Ignore if Kotlin compile task is not available
+        }
+    }
+    if (state.executed) {
+        configureAndroid()
+    } else {
+        afterEvaluate {
+            configureAndroid()
+        }
+    }
+}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
